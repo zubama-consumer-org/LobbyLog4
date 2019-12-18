a695d9f863b30de08bdee8ca5aa9af34038e25d4
ZUBAMA <ZUBAMA@WINL10577>
2019-12-18 13:21:15 -0500

    Add project files.

---
+++ LobbyLog4/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("LobbyLog4")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Consumer Reports")>
+<Assembly: AssemblyProduct("LobbyLog4")>
+<Assembly: AssemblyCopyright("Copyright © Consumer Reports 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("71e1dad3-cb12-4139-9f0d-113ce4a5de2b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
